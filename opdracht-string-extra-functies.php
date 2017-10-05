9a9c2a3c93da3decae535cf90221d2142e1a964a
imkeVr <ivanrompa@gmail.com>
2017-10-05 21:15:24 +0200

    deel 2 en deel 3 van opdracht bijgevoegd

--- opdracht-string-extra-functies.php
+++ opdracht-string-extra-functies.php
@@ -1,9 +1,19 @@
 
 <?php 
+#Deel 1
  $fruit             =   "kokosnoot";
  $fruitkarakters    =   strlen($fruit);
  $positie_O         =   strpos($fruit, "o");
 
+#Deel 2
+$_fruit             =   "ananas";
+$positie_a          =   strpos($_fruit, "a", 3);
+$fruitToUpper       =   strtoupper($_fruit);
+
+#Deel 3
+ $lettertje         =   "e";
+ $cijfertje         =    3;
+ $langsteWoord      =   "zandzeepsodemineralenwatersteenstralen";
 
 ?>
 
@@ -16,7 +26,18 @@
     <title>opdracht string extra functies</title>
 </head>
 <body>
+     
+    <h1>Deel 1 </h1>
+
     <p> Aantal karakters dat <?php echo $fruit ?> bevat = <?php  echo $fruitkarakters ?> .</p>
     <p>De eerte o in kokosnoot bevindt zich op de <?php  echo $positie_O ?>ste positie met index-0.</p>
+
+    <h2>Deel 2</h2>
+    <p>De laatste a in ananas bevindt zich op de <?php  echo $positie_a  ?>de positie met index-0.</p>
+    <p>ananas naar hoofdletters --> <?php echo $fruitToUpper  ?></p>
+
+    <h3>Deel3</h3>
+    <p>Vervang  alle e’s in zandzeepsodemineralenwatersteenstralen variable door 3's</p>
+    <p> <?php echo str_replace($lettertje, $cijfertje, $langsteWoord) ?></p>
 </body>
 </html>
