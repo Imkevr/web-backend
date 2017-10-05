8ccf02334e822cbce063a42b7b5f897bc4e7fd1b
imkeVr <ivanrompa@gmail.com>
2017-10-05 12:47:21 +0200

    comments

---
+++ opdracht-comments.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html >
+<head>
+   
+    <title>Document</title>
+</head>
+<body>
+  
+  <?php echo 'Imke Van Rompa' ?>
+
+  <?php #Imke Van Rompa , ivanrompa@gmail.com ?>
+
+    
+  
+
+</body>
+</html>
