1af957fa2f7afbba1c1b98c5b2a1a885f5c8d5e9
imkeVr <ivanrompa@gmail.com>
2017-10-11 12:47:10 +0200

    opdracht while deel 2

---
+++ opdracht-looping-statements-while-deel2.php
@@ -0,0 +1,41 @@
+
+<?php
+ $shoppinArr    =   array("tomaten", "paprika", "eieren", "melk");
+ $counter        =   0;
+ $arrayLength   =   count($shoppinArr);
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>BoodschappenLijstje</title>
+</head>
+<body>
+
+
+   
+    <h1>BoodschappenLijstje </h1>
+
+    <ul>
+
+        <?php while ($counter < $arrayLength):?>
+     <li><?php  echo $shoppinArr[$counter] ?></li>
+        <?php $counter ++ ?>
+        <?php endwhile ?>
+        
+    </ul>
+  
+        <!-- makkelijker met een foreach dan while 
+        <?php  foreach( $shoppinArr as $key => $val ):?>
+        <li><?php  echo $val ?></li>
+        <?php endforeach?>-->
+  
+
+
+</body>
+</html>
