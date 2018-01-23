a38f1cc33cb21cba9c1e3e46a08e98176177067f
imkeVr <ivanrompa@gmail.com>
2018-01-23 10:12:02 +0100

    opdracht sessions 3

--- opdracht-sessions-3.php
+++ opdracht-sessions-3.php
@@ -1,45 +1,48 @@
 <?php
-	session_start();
-    var_dump( $_POST );
-    var_dump( $_SESSION );
-    if ( isset( $_POST[ 'submit' ] ) )
-    {
-        $_SESSION['registrationData'][ 'deel2' ]['straat']  =   $_POST[ 'straat' ];
-        $_SESSION['registrationData'][ 'deel2' ]['nummer']  =   $_POST[ 'nummer' ];
-        $_SESSION['registrationData'][ 'deel2' ]['gemeente']  =   $_POST[ 'gemeente' ];
-        $_SESSION['registrationData'][ 'deel2' ]['postcode']  =   $_POST[ 'postcode' ];
-    }
-    $registrationData    =   $_SESSION['registrationData'];
+    session_start();
+    $_SESSION["straat"] = $_POST['straat'];
+    $_SESSION["nummer"] = $_POST['nummer'];
+    $_SESSION["gemeente"] = $_POST['gemeente'];
+    $_SESSION["postcode"] = $_POST['postcode'];
+    $email = $_SESSION["email"];
+    $nickname = $_SESSION["nickname"];
 ?>
+    <!doctype html>
+    <html>
 
-
-<!doctype html>
-<html>
     <head>
         <meta charset="utf-8">
-       
-        <title>Opdracht sessions 3</title>
-       
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Opdracht sessions 3 </title>
+        <link rel="stylesheet" href="http://web-backend.local/css/global.css">
+        <link rel="stylesheet" href="http://web-backend.local/css/facade.css">
     </head>
-    <body >
-        
-        <h1>Opdracht sessions 3</h1>
 
-        <a href="opdracht-sessions3.php?session=destroy">Vernietig sessie</a>
+    <body>
 
-        <h2>Overzicht</h2>
+        <h1>Overzichtspagina</h1>
 
         <ul>
-        <?php foreach( $registrationData as $deelKey => $deelArray ):  ?>
-
-            <?php foreach( $deelArray as $data => $value ):  ?>
-                <li>
-                    <?= $data ?>: <?= $value ?>
-                    <p><a href="phpoefening-021-<?= $deelKey ?>.php?focus=<?= $data ?>">wijzig</a></p>
-                </li>
-            <?php endforeach ?>
-        <?php endforeach ?>
+            <li>
+                <?php echo 'email: ' . $email . " | " ?><a href="opdracht-sessions-1.php?focus=email">wijzig</a>
+            </li>
+            <li>
+                <?php echo 'nickname: ' . $nickname . " | " ?><a href="opdracht-sessions-1.php?focus=nickname">wijzig</a>
+            </li>
+            <li>
+                <?php echo 'straat: ' . $_SESSION["straat"] . " | " ?><a href="opdracht-sessions-2.php?focus=straat">wijzig</a></li>
+            <li>
+                <?php echo 'nummer: ' . $_SESSION["nummer"] . " | " ?><a href="opdracht-sessions-2.php?focus=nummer">wijzig</a></li>
+            <li>
+                <?php echo 'gemeente: ' . $_SESSION["gemeente"] . " | " ?><a href="opdracht-sessions-2.php?focus=gemeente">wijzig</a></li>
+            <li>
+                <?php echo 'postcode: ' . $_SESSION["postcode"] . " | " ?><a href="opdracht-sessions-2.php?focus=postcode">wijzig</a></li>
         </ul>
-        
+
+
+        <a href="opdracht-sessions-1.php?session=destroy">Vernietig sessie</a>
+
+
     </body>
-</html>
+
+    </html>
