c71944fcf63efb9ef10d4448f8cac3a0acb4ded2
imkeVr <ivanrompa@gmail.com>
2018-01-23 09:50:52 +0100

    opdracht sessions

--- opdracht-sessions.php
+++ opdracht-sessions.php
@@ -1,13 +1,16 @@
-<?php 
-session_start();
-
-var_dump($_SESSION);
-
-$email		=	( isset( $_SESSION[ 'registrationData' ][ 'deel1' ][ 'email'] ) ) ? $_SESSION[ 'registrationData' ][ 'deel1' ][ 'email'] : '';
-$nickname		=	( isset( $_SESSION[ 'registrationData' ][ 'deel1' ][ 'nickname'] ) ) ? $_SESSION[ 'registrationData' ][ 'deel1' ][ 'nickname'] : '';
-
-
-
+<?php
+	session_start();
+	if ( isset( $_GET['session'] ) )
+    {
+        if ( $_GET['session']  == 'destroy' )
+        {
+            session_destroy( );
+            header( 'location: opdracht-sessions.php' );
+        }
+    }
+	var_dump( $_SESSION );
+	$email		=	( isset( $_SESSION[ 'registrationData' ][ 'deel1' ][ 'email'] ) ) ? $_SESSION[ 'registrationData' ][ 'deel1' ][ 'email'] : '';
+	$nickname	=	( isset( $_SESSION[ 'registrationData' ][ 'deel1' ][ 'nickname'] ) ) ? $_SESSION[ 'registrationData' ][ 'deel1' ][ 'nickname'] : '';
 ?>
 
 
@@ -17,29 +20,31 @@ $nickname		=	( isset( $_SESSION[ 'registrationData' ][ 'deel1' ][ 'nickname'] ) 
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Opdracht sessions</title>
+    <title>Opdracht sessions </title>
 </head>
 <body>
-    <h1>Deel 1 : registratiegegevens </h1>
 
-    <form action="opdracht-sessions.php" method="post">
+<h1>Opdracht sessions</h1>
 
-    <label for="email">e-mail</label>
-    <input type="email" name="email" id="email">
+<a href="opdracht-sessions.php?session=destroy">Destroy sessie</a>
 
-    <label for="nickname">nickname</label>
-    <input type="text" name="nickname" id="nickname">
+<h2>Deel1: registratiegegevens</h2>
 
+<form action="opdracht-sessions.php" methode="POST">
+    <ul>
+        <li>
+            <label for="email">email</label>
+            <input type="text" id="email" name="email" value="<?= $email ?>" placeholder="vul email in" <?= ( isset( $_GET['focus'] ) && $_GET['focus'] == "email" ) ? 'autofocus' : '' ?>>
+        </li>
+        <li>
+            <label for="nickname">nickname</label>
+            <input type="text" id="nickname" name="nickname" value="<?= $nickname ?>" placeholder="vul nickname in" <?= ( isset( $_GET['focus'] ) && $_GET['focus'] == "nickname" ) ? 'autofocus' : '' ?>>
+        </li>
+    </ul>
 
+    <input type="submit" name="Submit">
 
-    <input type="submit" value="submit">
-    
-    
-    </form>
-
-    
-
-
-
+</form>
 </body>
 </html>
+
